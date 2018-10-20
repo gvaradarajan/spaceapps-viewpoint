170da3a9327c109c5689c0cc1bca04a2507d962f
Gautham Varadarajan <gvaradarajan@vaynermedia.com>
2018-10-20 18:46:16 -0400

    more satellites

--- fetch.js
+++ fetch.js
@@ -15,25 +15,65 @@ function fetchSats() {
     return satsRequest;
 }
 
-function fetchSatelliteCoordinates(windowObject) {
-    let xmlData = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><DataRequest xmlns="http://sscweb.gsfc.nasa.gov/schema"><TimeInterval><Start>2013-09-15T15:53:00+05:00</Start><End>2013-09-18T15:53:00+05:00</End></TimeInterval><BFieldModel><InternalBFieldModel>IGRF-10</InternalBFieldModel><ExternalBFieldModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="Tsyganenko89cBFieldModel"><KeyParameterValues>KP3_3_3</KeyParameterValues></ExternalBFieldModel><TraceStopAltitude>100</TraceStopAltitude></BFieldModel><Satellites><Id>ace</Id><ResolutionFactor>2</ResolutionFactor></Satellites><OutputOptions><AllLocationFilters>true</AllLocationFilters><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>X</Component></CoordinateOptions><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>Y</Component></CoordinateOptions><CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem><Component>Z</Component></CoordinateOptions><MinMaxPoints>2</MinMaxPoints></OutputOptions></DataRequest>';
+function buildSatelliteRequest(startTime, endTime, satelliteIds) {
+    let xmlBegin = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+
+    let timeTag = `<TimeInterval><Start>${startTime}</Start><End>${endTime}</End></TimeInterval>`;
+
+    let intBFieldTag = '<InternalBFieldModel>IGRF-10</InternalBFieldModel>';
+    let externalBFieldTagStart = '<ExternalBFieldModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="Tsyganenko89cBFieldModel">';
+    let keyParamaterTag = '<KeyParameterValues>KP3_3_3</KeyParameterValues>';
+    let extBFieldTag = externalBFieldTagStart + keyParamaterTag + '</ExternalBFieldModel>';
+    let altTag = '<TraceStopAltitude>100</TraceStopAltitude>';
+    let bFieldTagInner = intBFieldTag + extBFieldTag + altTag;
+    let bFieldTag = `<BFieldModel>${bFieldTagInner}</BFieldModel>`;
+
+    idsTag = ''
+    for (satId of satIds) {
+        idsTag += `<Id>${satId}</Id>`
+    };
+
+    let satTag = `<Satellites>${idsTag}<ResolutionFactor>2</ResolutionFactor></Satellites>`;
+
+    let allFiltersTag = '<AllLocationFilters>true</AllLocationFilters>';
+    let coordinateTag = '';
+    let axes = ['X', 'Y', 'Z'];
+    let baseAxisTag = '<CoordinateOptions><CoordinateSystem>Gse</CoordinateSystem>';
+    for (axis of axes) {
+        let thisAxisTag = `${baseAxisTag}<Component>${axis}</Component`;
+        coordinateTag += thisAxisTag
+    };
+    let minMaxTag = '<MinMaxPoints>2</MinMaxPoints>';
+    let outputOptionsInner = allFiltersTag + coordinateTag + minMaxTag;
+    let outputOptionsTag = `<OutputOptions>%{outputOptionsInner}</OutputOptions`;
+
+    let requestXML = timeTag + bFieldTag + satTag + outputOptionsTag;
+    let withDataRequest = `<DataRequest xmlns="http://sscweb.gsfc.nasa.gov/schema">${requestXML}</DataInterval>`;
+    let result = xmlBegin + withDataRequest;
+    return result;
+}
+
+function fetchSatelliteCoordinates(windowObject, startTime, endTime) {
+    let xmlData = buildSatelliteRequest(
+        windowObject.satelliteIds, startTime, endTime);
     let requestParams = {
         contentType: 'application/xml',
         url: 'https://sscweb.sci.gsfc.nasa.gov/WS/sscr/2/locations',
         method: 'POST',
         data: xmlData
-    }
+    } 
     let locationsRequest = $.ajax(requestParams).then(
         (res) => {
             let parsed = parseXml(res);
             let satelliteId = parsed.Response.Result.Data.Id["#text"];
+            debugger
             let times = parsed.Response.Result.Data.Time;
             for (const [idx, time] of times.entries()) {
                 let coordinates = [
-                    parsed.Response.Result.Data.Coordinates.X[idx],
-                    parsed.Response.Result.Data.Coordinates.Y[idx],
-                    parsed.Response.Result.Data.Coordinates.Z[idx]
-                ]
+                    parsed.Result.Data.Coordinates.X[idx],
+                    parsed.Result.Data.Coordinates.Y[idx],
+                    parsed.Result.Data.Coordinates.Z[idx]
+                ];
                 windowObject.addSatelliteData(
                     satelliteId,
                     time["#text"],
--- main.js
+++ main.js
@@ -5,8 +5,13 @@ function main() {
     let wwd = new WorldWind.WorldWindow("canvasOne");
     worldWindow = new WorldWindowWrapper(wwd);
     worldWindow.setupWorldView();
-    fetchSats().then((res) => {worldWindow.satelliteIds = res;});
-    fetchSatelliteCoordinates(worldWindow);
+    fetchSats().then(
+        (res) => {
+            worldWindow.satelliteIds = res;
+            fetchSatelliteCoordinates(
+                worldWindow, '2013-09-15T15:53:00+05:00', '2013-09-18T15:53:00+05:00');
+        }
+    );
 }
 
 $(main);
